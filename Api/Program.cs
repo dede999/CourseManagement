1eaa541de80edc1257b769d09c1d7d507c76e93f
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-09-18 12:07:36 -0300

    First commit

---
+++ Api/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/health_check", () =>
+    {
+        var now = DateTime.Now;
+
+        return Results.Ok(new
+        {
+            Message = "The service is up and running",
+            Time = now.ToLongTimeString(),
+            Date = now.ToLongDateString()
+        });
+    })
+    .WithName("HealthCheck")
+    .WithOpenApi();
+
+app.Run();
