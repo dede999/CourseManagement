6d2196ce0b5d9d8dea732bf627e71f1953b08683
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-09-23 15:11:26 -0300

    Add user and courses relationship

--- Api/Infrastructure/DB/ApplicationContext.cs
+++ Api/Infrastructure/DB/ApplicationContext.cs
@@ -6,10 +6,16 @@ namespace Api.Infrastructure.DB;
 public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<Course> Courses { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>()
             .HasKey(u => u.Email);
+        
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Courses)
+            .WithOne(c => c.Creator)
+            .HasForeignKey(c => c.CreatorEmail);
     }
 }
