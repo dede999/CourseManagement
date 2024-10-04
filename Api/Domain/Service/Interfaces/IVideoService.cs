5659ef7983434d59c8fe1f5ea392c6c77439452d
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-10-04 18:14:21 -0300

    Create the video service interface

---
+++ Api/Domain/Service/Interfaces/IVideoService.cs
@@ -0,0 +1,15 @@
+using Api.Domain.DTOs;
+using Api.Domain.DTOs.Video;
+using Api.Domain.Entities;
+using Api.Infrastructure.Request;
+
+namespace Api.Domain.Service.Interfaces;
+
+public interface IVideoService
+{
+    Task<RetrieveResponse<VideoBlockDto[]>> AllVideos(Guid courseCode);
+    Task<ValidationResponse<Video>> CreateVideo(VideoPersistenceDto video);
+    Task<RetrieveResponse<Video?>> GetVideo(Guid code);
+    Task<ValidationResponse<Video?>> UpdateVideo(Guid code, VideoPersistenceDto video);
+    void DeleteVideo(Guid code);
+}
